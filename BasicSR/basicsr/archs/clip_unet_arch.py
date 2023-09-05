f8f57179da228b16fafa80d62c9170787f4129c5
XYChen21 <coco.001121@gmail.com>
2023-09-05 16:39:15 +0800

    first commit

---
+++ BasicSR/basicsr/archs/clip_unet_arch.py
@@ -0,0 +1,97 @@
+from collections import OrderedDict
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch import nn
+from basicsr.archs.clip_arch import build_model
+from basicsr.utils.registry import ARCH_REGISTRY
+from basicsr.utils.upsample import TransposedConvUp
+
+
+#TODO:CLIP preprocess
+# def _transform(n_px): n_px = model.visual.input_resolution = 224
+#     return Compose([
+#         Resize(n_px, interpolation=BICUBIC),
+#         CenterCrop(n_px),
+#         _convert_image_to_rgb,
+#         ToTensor(),
+#         Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+#     ])
+
+@ARCH_REGISTRY.register()
+class CLIPUNetGenerator(nn.Module):
+    def __init__(self, num_out_ch=3, scale=4) -> None:
+        super().__init__()
+        self.scale = scale
+        clip_path = '/Users/x/Desktop/BasicSR/experiments/pretrained_models/CLIP/RN50.pt'
+        # print(clip_path)
+        with open(clip_path, 'rb') as opened_file:
+            model = torch.jit.load(opened_file, map_location="cpu").eval()
+            clip = build_model(model.state_dict()).visual ## we only need visual part?
+        # if device == "cpu":
+        clip.float()
+        clip.requires_grad_(False)  ## build_model already return model.eval()?
+        self.channels = {'down2': 64}
+        for i in range(2,6):
+            factor = 2**i
+            self.channels['down'+str(factor)] = 64*factor
+
+        self.clip_feature = nn.ModuleList()
+        first_down = nn.Sequential(
+            clip.conv1, clip.bn1, clip.relu1,
+            clip.conv2, clip.bn2, clip.relu2,
+            clip.conv3, clip.bn3, clip.relu3,
+        )
+        self.clip_feature.append(first_down) #//2
+        self.clip_feature.append(nn.Sequential(clip.avgpool, clip.layer1)) #//4
+        self.clip_feature.append(clip.layer2) #//8
+        self.clip_feature.append(clip.layer3) #//16
+        self.clip_feature.append(clip.layer4) #//32
+
+        self.up_layers = nn.ModuleList()
+        self.fuse_convs = nn.ModuleList()
+        self.bn = nn.ModuleList()
+        for i in range(5, 1, -1):
+            factor = 2**i
+            in_chan = self.channels['down'+str(factor)]
+            out_chan = self.channels['down'+str(factor//2)]
+            self.up_layers.append(TransposedConvUp(in_chan, out_chan, factor=2))
+            self.fuse_convs.append(nn.Conv2d(out_chan*2, out_chan, 3, 1, 1))
+            self.bn.append(nn.BatchNorm2d(out_chan))
+        # upsample (from 224//2=112 to 224)
+        # out_chan=64
+        if self.scale == 4:
+            self.up_layers.append(TransposedConvUp(out_chan, out_chan, factor=2))
+        self.conv_hr = nn.Conv2d(out_chan, out_chan, 3, 1, 1)
+        self.conv_last = nn.Conv2d(out_chan, num_out_ch, 3, 1, 1)
+
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+
+
+    def forward(self, lq):
+        lq = F.interpolate(lq, scale_factor=self.scale, mode='bicubic')
+        x0 = self.clip_feature[0](lq)
+        x1 = self.clip_feature[1](x0)
+        x2 = self.clip_feature[2](x1)
+        x3 = self.clip_feature[3](x2)
+        x4 = self.clip_feature[4](x3)
+
+        x5 = self.up_layers[0](x4)
+        x5 = self.fuse_convs[0](torch.cat((x3, x5), dim=1))
+        x5 = self.lrelu(self.bn[0](x5))
+        x6 = self.up_layers[1](x5)
+        x6 = self.fuse_convs[1](torch.cat((x2, x6), dim=1))
+        x6 = self.lrelu(self.bn[1](x6))
+        x7 = self.up_layers[2](x6)
+        x7 = self.fuse_convs[2](torch.cat((x1, x7), dim=1))
+        x7 = self.lrelu(self.bn[2](x7))
+        x8 = self.up_layers[3](x7)
+        x8 = self.fuse_convs[3](torch.cat((x0, x8), dim=1))
+        x8 = self.lrelu(self.bn[3](x8))
+
+        if self.scale == 4:
+            x9 = self.up_layers[4](x8)
+            out = self.conv_last(self.lrelu(self.conv_hr(x9)))
+        else: # if scale==2, from 56->112
+            out = self.conv_last(self.lrelu(self.conv_hr(x8)))
+        return out
---
+++ BasicSR/basicsr/utils/upsample.py
@@ -0,0 +1,186 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+from basicsr.ops.upfirdn2d.upfirdn2d import upfirdn2d
+# from .sr_backbone import default_init_weights
+
+
+class PixelShuffleUp(nn.Module):
+    """Pixel Shuffle upsample layer.
+
+    Copied from
+    https://github.com/open-mmlab/mmagic/blob/107647a8edab3186685dc88af491d9795083523b/mmagic/models/archs/upsample.py#L9
+
+    Args:
+        in_channels (int): Number of input channels.
+        out_channels (int): Number of output channels.
+        scale_factor (int): Upsample ratio.
+        upsample_kernel (int): Kernel size of Conv layer to expand channels.
+
+    Returns:
+        Upsampled feature map.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int, scale_factor=2,
+                 upsample_kernel=3):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.scale_factor = scale_factor
+        self.upsample_kernel = upsample_kernel
+        self.upsample_conv = nn.Conv2d(
+            self.in_channels,
+            self.out_channels * scale_factor * scale_factor,
+            self.upsample_kernel,
+            padding=(self.upsample_kernel - 1) // 2)
+        # self.init_weights()
+
+    # def init_weights(self) -> None:
+    #     """Initialize weights for PixelShufflePack."""
+    #     default_init_weights(self, 1)
+
+    def forward(self, x: Tensor) -> Tensor: # x.shape=(n,c,h,w)
+        x = self.upsample_conv(x)
+        x = F.pixel_shuffle(x, self.scale_factor)
+        return x
+
+
+class InterpolateConvUp(nn.Module):
+    def __init__(self, in_chan, out_chan, scale_factor=2, kernel=3) -> None:
+        super().__init__()
+        self.in_channels = in_chan
+        self.out_channels = out_chan
+        self.scale_factor = scale_factor
+        self.upsample_kernel = kernel
+        self.conv = nn.Conv2d(in_chan, out_chan, kernel, 1, 1)
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+
+    def forward(self, x):
+        out = self.conv(F.interpolate(x, scale_factor=self.scale_factor, mode='nearest'))
+        return self.lrelu(out)
+
+
+class TransposedConvUp(nn.Module):
+    def __init__(self, in_chan, out_chan, factor=2, kernel=2, blur_kernel=[1,3,3,1]) -> None:
+        super().__init__()
+        self.in_channels = in_chan
+        self.out_channels = out_chan
+        self.scale_factor = factor
+        self.kernel = kernel
+        self.blur = UpsampleUpFIRDn(kernel, True, blur_kernel, factor=2)
+        self.up_conv = nn.ConvTranspose2d(in_chan, out_chan, kernel, stride=2)
+        self.conv = nn.Conv2d(out_chan, out_chan, kernel, 1, 1)
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+
+    def forward(self, x):
+        out = self.lrelu(self.up_conv(x))
+        # out = self.blur(out)
+        # out = self.lrelu(self.conv(out))
+        return out
+
+
+class ToRGB(nn.Module):
+    '''
+    Borrowed from
+    https://github.com/open-mmlab/mmagic/blob/107647a8edab3186685dc88af491d9795083523b/mmagic/models/editors/stylegan2/stylegan2_modules.py#L396
+
+    '''
+
+    def __init__(self, in_chan, out_chan=3, upsample=True, blur_kernel=[1, 3, 3, 1],
+                 padding=None, kernel_size=1, out_fp32=True,
+                 fp16_enabled=False, conv_clamp=256):
+        super().__init__()
+
+        if upsample:
+            self.upsample = UpsampleUpFIRDn(kernel_size, False, blur_kernel, factor=2)
+        # add support for fp16
+        # self.fp16_enabled = fp16_enabled
+        # self.conv_clamp = float(conv_clamp)
+        self.padding = padding if padding else (kernel_size // 2)
+        self.conv = nn.Conv2d(in_chan, out_chan, 1, stride=1, padding=self.padding)
+
+        self.bias = nn.Parameter(torch.zeros(1, out_chan, 1, 1))
+
+        # enforece the output to be fp32 (follow Tero's implementation)
+        self.out_fp32 = out_fp32
+
+    # @auto_fp16(apply_to=('x', 'style'))
+    def forward(self, x, style, skip=None):
+        # with autocast(enabled=self.fp16_enabled):
+        out = self.conv(x, style)
+        out = out + self.bias.to(x.dtype)
+
+        # if self.fp16_enabled:
+        #     out = torch.clamp(
+        #         out, min=-self.conv_clamp, max=self.conv_clamp)
+
+            # Here, Tero adopts FP16 at `skip`.
+        if skip is not None:
+            if hasattr(self, 'upsample'):
+                skip = self.upsample(skip)
+            out = out + skip
+        if self.out_fp32:
+            out = out.to(torch.float32)
+        return out
+
+def make_kernel(k):
+    k = torch.tensor(k, dtype=torch.float32)
+    if k.ndim == 1:
+        k = k[None, :] * k[:, None]
+
+    k /= k.sum()
+
+    return k
+
+class UpsampleUpFIRDn(nn.Module):
+    """UpFIRDn for Upsampling.
+
+    Borrowed from
+    https://github.com/open-mmlab/mmagic/blob/107647a8edab3186685dc88af491d9795083523b/mmagic/models/editors/stylegan1/stylegan1_modules.py#L191
+
+    https://github.com/open-mmlab/mmagic/blob/107647a8edab3186685dc88af491d9795083523b/mmagic/models/editors/stylegan2/stylegan2_modules.py#L43
+
+    This module is used in the ``to_rgb`` layers for upsampling the images,
+    or rightly after upsampling operation in StyleGAN2 to build a blurry layer.
+
+    Args:
+        kernel (Array): Blur kernel/filter used in UpFIRDn.
+        factor (int, optional): Upsampling factor. Defaults to 2.
+    """
+
+    def __init__(self, up_kernel_size, is_blur, blur_kernel=[1,3,3,1], factor=2):
+        super().__init__()
+        blur_kernel = make_kernel(blur_kernel)
+        self.factor = factor
+        self.is_blur = is_blur
+        if factor > 1:
+            blur_kernel = blur_kernel * (factor**2)
+        self.register_buffer('blur_kernel', blur_kernel)
+
+        p = blur_kernel.shape[0] - factor - (up_kernel_size - 1)
+
+        pad0 = (p + 1) // 2 + factor - 1
+        pad1 = p // 2
+        if is_blur:
+            pad1 += 1
+
+        self.pad = (pad0, pad1)
+
+    def forward(self, x):
+        if self.is_blur:
+            up = 1
+        else:
+            up = self.factor
+        out = upfirdn2d(
+            x,
+            self.blur_kernel.to(x.dtype),
+            up=up,
+            down=1,
+            pad=self.pad
+        )
+
+        return out
+
+
+
